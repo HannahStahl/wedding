7366333eb1b7c5827fe2031b6ec624c6bd31772b
Hannah Stahl <hls62@cornell.edu>
2023-05-14 22:25:45 -0400

    Start building out home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Home = () => (
-  <img src="/photos/home.jpg" alt="Chuck and Hannah" className="home-img" />
+  <>
+    <img src="/images/engagement.jpg" alt="Chuck and Hannah" className="home-img" />
+    <div className="our-story">
+      <img src="/images/arrow-1.png" alt="Arrow 1" />
+      <div className="story-row left-align">
+        <div>
+          <h1>It began in snowy upstate New York... (sort of)</h1>
+          <p>Chuck and Hannah like to think fate first tried to get them together at Cornell when they both joined the class of 2016. The very first night of freshman year, they unknowingly went to the same memorable party, where the porch collapsed under the weight of hundreds of college students. Then throughout the years, they both found themselves at Reis Tennis Center, where Hannah practiced with the club team and Chuck taught lessons to local kids. During junior year, they even ended up at the same Slope Day party, where Hannah witnessed Chuck performing a very mean but impressively written rap about a mutual friend of theirs named Ian. Despite crossing paths about a hundred times, however, they never actually met at Cornell.</p>
+        </div>
+        <div>
+          <img src="/images/snowy-ithaca.jpg" alt="Snowy Ithaca" className="story-img" />
+          <img src="/images/cornell-hockey.jpg" alt="Cornell Hockey" className="story-img" />
+          <img src="/images/clocktower.jpg" alt="Clocktower" className="story-img" />
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default Home;
