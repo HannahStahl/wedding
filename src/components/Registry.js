1424a724d95224e3529e74919b9862181c8b8634
Hannah Stahl <hls62@cornell.edu>
2023-06-17 00:18:39 -0400

    Add registry link

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Registry = () => (
-  <div className="page">Registry coming soon</div>
+  <div className="page">
+    Please <a href="https://zola.com/registry/chuckandhannah2023" target="_blank" rel="noopener noreferrer">click here</a> to view our registry!
+  </div>
 );
 
 export default Registry;
