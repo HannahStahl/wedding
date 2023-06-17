d80e4a12bc3851b37213fddf2e6b3c1c63ce6961
Hannah Stahl <hls62@cornell.edu>
2023-06-16 23:59:15 -0400

    Add RSVP link

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const RSVP = () => (
-  <div className="page">RSVP coming soon</div>
+  <div className="page">
+    Please <a href="https://burtonstahlwedding.rsvpify.com" target="_blank" rel="noopener noreferrer">click here</a> to fill out our RSVP form!
+  </div>
 );
 
 export default RSVP;
